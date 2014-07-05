dd2096c4776cef4be94b053d2eb3f36d048b6859
Felix Bade <felix@bade.fi>
2014-07-06 01:42:08 +0300

    Log disconnections

--- client_handler.py
+++ client_handler.py
@@ -15,51 +15,55 @@ class ClientHandler(Thread):
         self.start()
 
     def run(self):
-        # Tell the new client who are present
-        self.connection.send('Currently online:\n')
-        other_clients = [x for x in self.server.clients if x is not self]
-        if other_clients:
-            longest_nick_length = max(len(client.nick) for client in other_clients)
-            for client in other_clients:
-                nick = client.nick
-                host = client.address[0]
-                port = client.address[1]
-                space = ' ' * (longest_nick_length + 1 - len(nick))
-                self.connection.send('%s%s%s:%d\n' % (nick, space, host, port))
-       
-        # Ask for a nick
-        self.connection.send('What nick would you like to use?\n')
-        self.nick = self.connection.makefile().readline().strip('\n')
-        
-        if self.nick in [client.nick for client in other_clients]:
-            self.connection.send('That nick is already in use.\n')
-            self.close()
-            return
-        
-        if len(self.nick) > 24:
-            self.connection.send('Your nick cannot be longer than 24 characters\n')
-            self.close()
-            return
-        
-        # Greet other clients
-        host = self.address[0]
-        port = self.address[1]
-        self.broadcast('New user: %s (%s:%d)\n' % (self.nick, host, port))
-        
-        # Broadcast every message forever
-        while True:
-            line = self.connection.makefile().readline()
-            if line == '':
-                self.broadcast('Quit: %s\n' % self.nick)
+        try:
+            # Tell the new client who are present
+            self.connection.send('Currently online:\n')
+            other_clients = [x for x in self.server.clients if x is not self]
+            if other_clients:
+                longest_nick_length = max(len(client.nick) for client in other_clients)
+                for client in other_clients:
+                    nick = client.nick
+                    host = client.address[0]
+                    port = client.address[1]
+                    space = ' ' * (longest_nick_length + 1 - len(nick))
+                    self.connection.send('%s%s%s:%d\n' % (nick, space, host, port))
+           
+            # Ask for a nick
+            self.connection.send('What nick would you like to use?\n')
+            self.nick = self.connection.makefile().readline().strip('\n')
+            
+            if self.nick in [client.nick for client in other_clients]:
+                self.connection.send('That nick is already in use.\n')
                 self.close()
-                break
+                return
+            
+            if len(self.nick) > 24:
+                self.connection.send('Your nick cannot be longer than 24 characters\n')
+                self.close()
+                return
+            
+            # Greet other clients
             host = self.address[0]
             port = self.address[1]
-            self.broadcast('%s: %s' % (self.nick, line))
+            self.broadcast('New user: %s (%s:%d)\n' % (self.nick, host, port))
+            
+            # Broadcast every message forever
+            while True:
+                line = self.connection.makefile().readline()
+                if line == '':
+                    self.broadcast('Quit: %s\n' % self.nick)
+                    self.close()
+                    break
+                host = self.address[0]
+                port = self.address[1]
+                self.broadcast('%s: %s' % (self.nick, line))
+        except:
+            self.close()
     
     def close(self):
         self.connection.close()
         self.server.clients.remove(self)
+        print 'Lost connection %s:%d' % self.address
 
     def broadcast(self, message):
         for client in self.server.clients:
